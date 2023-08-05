05787686040a6b9a5df76a6bff44b588ead27a6e
NischalKShaj <nischalkshaj5@gmail.com>
2023-08-05 23:20:10 +0530

    Updted files

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 const session = require('express-session')
 
-const homeRouter = require('./routes/home')
-const userRouter = require('./routes/user')
+const router = require("./routes/auth");
 
 app.set('view engine','ejs')
 app.set('views','views')
+
+app.use((req, res, next)=>{
+    res.set("cache-control","no-store")
+    next();
+})
+
 
 app.use(session({
     secret :"seceretkey",
@@ -19,13 +23,26 @@ app.use(session({
 app.use(express.urlencoded({extended : true}))
 app.use(express.static('./public'))
 
-
-app.use('/',homeRouter)
-app.use('/logout',userRouter)
-
-app.use((req,res)=>{
-    res.status(404).send("404 page not found")
+app.get("/", (req, res) => {
+    if(req.session.user) {
+        res.redirect("/user");
+    } else {
+        res.render("home")
+    }
 })
+
+app.get("/user", (req, res) => {
+
+    if(req.session.user) {
+       
+        res.render("user", {username: req.session.user})
+    } else {
+        res.redirect("/")
+    }
+})
+
+app.use("/auth", router)
+
 
 app.listen(3000,()=>{
     console.log("server started...");
---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+const credential = {
+    username: "nischal",
+    password: "red"
+}
+
+router.post("/login", (req, res) => {
+    const {username, password} = req.body;
+
+    if(username === credential.username && password === credential.password) {
+        req.session.user = username;
+        res.redirect("/user")
+    } else {
+        res.render("home", {err:true})
+    }
+})
+
+router.post("/logout", (req, res) => {
+    req.session.destroy(function (err) {
+        if (err) {
+          console.log(err);
+          res.send("Error");
+        } else {
+          console.log("test");
+          res.status(200).send('Okay');
+        }
+      });
+})
+
+
+
+module.exports = router;
